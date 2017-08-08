ea5fbe66c4ff627dc0a9007ae927f4a6d7623916
jerseyhokie <jerseyhokie@users.noreply.github.com>
2017-08-07 23:33:38 -0500

    Initial Upload of Files

---
+++ README.md
@@ -0,0 +1,24 @@
+# Entertainment Center
+
+## Description
+Simple movie poster website, uses the Udacity `Fresh Tomatoes.py` code
+and `media.py` code to create a simple website of movies
+hard coded into `entertainment_center.py`
+
+## Files
+- media.py
+- entertainment_center.py
+- fresh_tomatoes.py
+
+## Usage
+Run the `entertainment_center.py` file within a python shell, which will
+open a browser and display the movies
+
+## Links
+`Fresh Tomatoes.py` is available [here](https://github.com/udacity/ud036_StarterCode/blob/master/fresh_tomatoes.py)
+
+### License
+MIT License
+
+### Author
+Chris Baines
---
+++ media.py
@@ -0,0 +1,43 @@
+import webbrowser
+
+class Movie():
+    """ This class provides a way to store a movies related information
+
+    Attributes:
+        title: A string of the movies title
+        storyline: A string of the movies main storyline or description
+        poster_image_url: A string containing the movies poster via IMDB
+        trailer_youtube_url: A string to the best quality trailer for the movie on YouTube
+        VALID_RATINGS: a string of the movies rating - G, PG, PG-13 or R
+    
+    """
+    
+    VALID_RATINGS = ["G","PG","PG-13","R"]
+    
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, rating):
+        """ Inits Movie with all required attributes """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.VALID_RATINGS = rating
+
+    def show_title(self):
+        """ Print the title """
+        print self.title
+
+    def show_storyline(self):
+        """ Print the storyline """
+        print self.storyline
+
+    def show_poster(self):
+        """ Open a webbrowser window with the movie poster """
+        webbrowser.open(self.poster_image_url)
+
+    def show_trailer(self):
+        """ Open a webbrowser window with the movie trailer """
+        webbrowser.open(self.trailer_youtube_url)    
+
+    def show_rating(self):
+        """ Print the rating """
+        print self.VALID_RATINGS
